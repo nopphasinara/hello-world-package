391e121038f9d6a3c8b689c9ff0d4346e3ee69a8
nopphasinara <nopphasin.arayasirikul@gmail.com>
2023-09-29 14:49:52 +0700

    Fix class

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -1,9 +1,13 @@
 <?php
- 
+
 namespace HelloWorldPackage;
- 
+
 class HelloWorld
 {
+    public static function __construct() {
+      echo __CLASS__;
+    }
+
     public static function sayHello()
     {
         echo "Hello World";
